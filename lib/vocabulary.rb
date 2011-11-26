b4176a42355728f0beb2ca0973af8cbabe30222c
Kir <shatrov@me.com>
2011-11-27 01:06:43 +0400

    Exception handling

--- lib/vocabulary.rb
+++ lib/vocabulary.rb
@@ -2,16 +2,27 @@ require 'httparty'
 require "vocabulary/version"
 
 module Vocabulary
+  class LookUpError < ::StandardError
+    attr_accessor :summary
+    def initialize(summary)
+      super "Error while parsing Dictionary API response, may be you requested non-existent word. Response: #{summary}"
+      @summary = summary
+    end
+  end
+  
   class Word
     attr_accessor :part_of_speech, :meanings
     
     def initialize summary
-      raise ArgumentError, "Bad summary" unless summary
-      @part_of_speech = summary["primaries"][0]["entries"][0]["labels"][0]["text"]
+      begin
+        @part_of_speech = summary["primaries"][0]["entries"][0]["labels"][0]["text"]
       
-      @meanings = []
-      summary["primaries"][0]["entries"][0]["entries"].each do |meaning|
-        @meanings << meaning["terms"][0]["text"]
+        @meanings = []
+        summary["primaries"][0]["entries"][0]["entries"].each do |meaning|
+          @meanings << meaning["terms"][0]["text"]
+        end
+      rescue
+        raise Vocabulary::LookUpError.new(summary)
       end
     end
   end
@@ -28,8 +39,8 @@ module Vocabulary
       options[:query] = {
         :callback => "substr",
         :q => word,
-        :sl => target_lang,
-        :tl => source_lang
+        :sl => target_lang, # source lang
+        :tl => source_lang # target lang
       }
       
       response = self.class.get("/dictionary/json", options)
@@ -43,6 +54,7 @@ module Vocabulary
   
   def self.lookup(word, source_lang, target_lang = nil)
     target_lang ||= :en
+    raise ArgumentError, "Word can't be blank" unless word.present?
     data = Vocabulary::Dictionary.new.get(word, source_lang.to_s, target_lang.to_s)
     Vocabulary::Word.new(data)
   end
